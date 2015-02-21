f92522353f3dd688cb73be014610ef504c8216d1
trenpixster <nizar.venturini@gmail.com>
2015-02-21 02:50:38 +0000

    Fix indentation
    Wtf happened there? ^^

--- lib/addict/interactors/addict_manager_interactor.ex
+++ lib/addict/interactors/addict_manager_interactor.ex
@@ -68,85 +68,85 @@ defmodule Addict.BaseManagerInteractor do
         {:error, "invalid recovery hash"}
       end
 
-        @doc """
-          Resets the password for the user with the given `recovery_hash`.
-        """
-        def reset_password(recovery_hash, password, password_confirm, repo \\ Addict.Repository, password_interactor \\ Addict.PasswordInteractor)  when password == password_confirm do
-          hash = password_interactor.generate_hash(password)
-          repo.find_by_recovery_hash(recovery_hash)
-          |> reset_user_password(hash, repo)
+      @doc """
+        Resets the password for the user with the given `recovery_hash`.
+      """
+      def reset_password(recovery_hash, password, password_confirm, repo \\ Addict.Repository, password_interactor \\ Addict.PasswordInteractor)  when password == password_confirm do
+        hash = password_interactor.generate_hash(password)
+        repo.find_by_recovery_hash(recovery_hash)
+        |> reset_user_password(hash, repo)
+      end
+
+      #
+      # Private functions
+      #
+
+      defp reset_user_password(nil,_,_) do
+        {:error, "invalid recovery hash"}
+      end
+
+      defp reset_user_password({:error, message},_,_) do
+        {:error, message}
+      end
+
+      defp reset_user_password(user, hash, repo) do
+        repo.change_password(user, hash)
+      end
+
+
+      defp prepare_password_recovery(email, repo) do
+        hash = PasswordInteractor.generate_random_hash
+        repo.find_by_email(email)
+        |> repo.add_recovery_hash(hash)
+      end
+
+      defp validate_params(nil) do
+        throw "Unable to create user, invalid hash: nil"
+      end
+
+      defp validate_params(user_params) do
+        case is_nil(user_params["email"])
+             or is_nil(user_params["password"])
+             or is_nil(user_params["username"]) do
+               false -> user_params
+               true -> throw "Unable to create user, invalid hash. Required params: email, password, username"
+             end
+      end
+
+      defp create_username(hash, user_params, repo) do
+        user_params = Map.delete(user_params, "password")
+        |> Map.put("hash", hash)
+        repo.create(user_params)
+      end
+
+      defp send_password_recovery_email({:ok, nil}, _) do
+        {:error, "Unable to send recovery e-mail"}
+      end
+
+      defp send_password_recovery_email({:ok, user}, mailer) do
+        result = mailer.send_password_recovery_email(user)
+        IO.puts "sent recovery email"
+        case result do
+          {:ok, _} -> {:ok, user}
+          {:error, message} -> {:error, message}
         end
+      end
 
-        #
-        # Private functions
-        #
+      defp send_password_recovery_email({:error, _}, _) do
+        {:error, "Unable to send recovery e-mail"}
+      end
 
-        defp reset_user_password(nil,_,_) do
-          {:error, "invalid recovery hash"}
+      defp send_welcome_email({:ok, user}, mailer) do
+        result = mailer.send_welcome_email(user)
+        case result do
+          {:ok, _} -> {:ok, user}
+          {:error, message} -> {:error, message}
         end
+      end
 
-        defp reset_user_password({:error, message},_,_) do
-          {:error, message}
-        end
-
-        defp reset_user_password(user, hash, repo) do
-          repo.change_password(user, hash)
-        end
-
-
-        defp prepare_password_recovery(email, repo) do
-          hash = PasswordInteractor.generate_random_hash
-          repo.find_by_email(email)
-          |> repo.add_recovery_hash(hash)
-        end
-
-        defp validate_params(nil) do
-          throw "Unable to create user, invalid hash: nil"
-        end
-
-        defp validate_params(user_params) do
-          case is_nil(user_params["email"])
-               or is_nil(user_params["password"])
-               or is_nil(user_params["username"]) do
-                 false -> user_params
-                 true -> throw "Unable to create user, invalid hash. Required params: email, password, username"
-               end
-        end
-
-        defp create_username(hash, user_params, repo) do
-          user_params = Map.delete(user_params, "password")
-          |> Map.put("hash", hash)
-          repo.create(user_params)
-        end
-
-        defp send_password_recovery_email({:ok, nil}, _) do
-          {:error, "Unable to send recovery e-mail"}
-        end
-
-        defp send_password_recovery_email({:ok, user}, mailer) do
-          result = mailer.send_password_recovery_email(user)
-          IO.puts "sent recovery email"
-          case result do
-            {:ok, _} -> {:ok, user}
-            {:error, message} -> {:error, message}
-          end
-        end
-
-        defp send_password_recovery_email({:error, _}, _) do
-          {:error, "Unable to send recovery e-mail"}
-        end
-
-        defp send_welcome_email({:ok, user}, mailer) do
-          result = mailer.send_welcome_email(user)
-          case result do
-            {:ok, _} -> {:ok, user}
-            {:error, message} -> {:error, message}
-          end
-        end
-
-        defp send_welcome_email({:error, message}, _) do
-          {:error, message}
-        end
+      defp send_welcome_email({:error, message}, _) do
+        {:error, message}
+      end
     end
   end
 end
