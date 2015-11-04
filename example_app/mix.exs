e13752c2904bf71aece4904094e31e2a6c53254c
Michael Chavez <bigbassroller@gmail.com>
2015-11-03 21:09:39 -0800

    Bump ecto from 0.9 to 1.0.

--- example_app/mix.exs
+++ example_app/mix.exs
@@ -30,7 +30,7 @@ defmodule ExampleApp.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [{:phoenix, "~> 0.16"},
-     {:phoenix_ecto, "~> 0.9"},
+     {:phoenix_ecto, "~> 1.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.0"},
      {:phoenix_live_reload, "~> 0.6", only: :dev},
