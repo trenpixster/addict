96d1696fd4be2a6a5e26ff5f14005da416281db4
Nizar Venturini <nizar.venturini@gmail.com>
2016-06-02 14:27:22 +0100

    Update example app with DELETE endpoint for /logout

--- example_app/web/views/addict_view.ex
+++ example_app/web/views/addict_view.ex
@@ -1,6 +1,6 @@
 defmodule Addict.AddictView do
   use Phoenix.HTML
   use Phoenix.View, root: "web/templates/"
-  import Phoenix.Controller, only: [get_csrf_token: 0, get_flash: 2, view_module: 1]
+  import Phoenix.Controller, only: [view_module: 1]
   import ExampleApp.Router.Helpers
 end
