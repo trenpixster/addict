f436ef05f8c1c085cbe85234752a593fc3089372
Nizar Venturini <nizar.venturini@gmail.com>
2016-06-02 14:33:59 +0100

    Bump version to 0.2.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Addict.Mixfile do
 
   def project do
     [app: :addict,
-     version: "0.2.4",
+     version: "0.2.5",
      elixir: "~> 1.2",
      description: description,
      package: package,
